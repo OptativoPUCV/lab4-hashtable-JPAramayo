0311f377d67b79954d32bcae414c09d70c31c317
JPAramayo <162994916+JPAramayo@users.noreply.github.com>
2024-04-26 00:05:12 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -122,7 +122,7 @@ Pair * firstMap(HashMap * map) {
 }
 
 Pair * nextMap(HashMap * map) {
-  for (long i = map->current; i < map->capacity; i++) {
+  for (long i = map->current + 1; i < map->capacity; i++) {
     if (map->buckets[i] != NULL && map->buckets[i]->key != NULL) {
       map->current = i;
       return map->buckets[i];
